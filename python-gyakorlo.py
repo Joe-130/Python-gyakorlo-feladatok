9445fdeccff4594eeb5e48a216c7d127ce8c3770
admin <hlivaj@kkszki.hu>
2025-11-29 14:30:06 +0100

    A harmadik feladat kész

--- python-gyakorlo.py
+++ python-gyakorlo.py
@@ -19,3 +19,23 @@ Kérj be egy számot, és döntsd el, hogy páros-e.
 #     print("A beírt szám a 0")
 # else:
 #     print("A beírt szám páratlan")
+
+#--------------------------------------------------------------
+
+"""
+Kérj be három számot, és írd ki melyik a legnagyobb.
+"""
+
+# szam1 = int(input("Kérlek adj meg egy számot: "))
+# szam2 = int(input("Kérlek adj meg még egy számot: "))
+# szam3 = int(input("Kérlek adj meg még egy számot: "))
+# if szam1 > szam2 and szam1 > szam3:
+#     print(f"Az első szám a {szam1} a legnagyobb")
+# elif szam2 > szam3:
+#     print(f"A második szám a {szam2} a legnagyobb")
+# else:
+#     if szam1 == szam2 and szam1 == szam3 and szam3 == szam2:
+#         print("A három szám egyforma")
+#     else:
+#         print(f"A harmadik szám a {szam3} a legnagyobb")
+
