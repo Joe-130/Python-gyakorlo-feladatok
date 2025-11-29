5a588e13196ba81186a719ab56244ece6608eda4
admin <hlivaj@kkszki.hu>
2025-11-29 14:15:15 +0100

    Az első feladat kész

---
+++ python-gyakorlo.py
@@ -0,0 +1,7 @@
+"""
+Kérj be két számot, és írd ki az összegüket.
+"""
+
+szam1 = int(input("Kérlek Adj meg egy számot: "))
+szam2 = int(input("Kérlek Adj meg még egy számot: "))
+print(f"A két szám összege {szam1+szam2}.")
