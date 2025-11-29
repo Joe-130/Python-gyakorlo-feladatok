dfe851e04e0b16a5b33bd39705f09b759409377c
admin <hlivaj@kkszki.hu>
2025-11-29 14:20:39 +0100

    A második feladat kész.

--- python-gyakorlo.py
+++ python-gyakorlo.py
@@ -2,6 +2,20 @@
 Kérj be két számot, és írd ki az összegüket.
 """
 
-szam1 = int(input("Kérlek Adj meg egy számot: "))
-szam2 = int(input("Kérlek Adj meg még egy számot: "))
-print(f"A két szám összege {szam1+szam2}.")
+# szam1 = int(input("Kérlek Adj meg egy számot: "))
+# szam2 = int(input("Kérlek Adj meg még egy számot: "))
+# print(f"A két szám összege {szam1+szam2}.")
+
+#--------------------------------------------------------------
+
+""""
+Kérj be egy számot, és döntsd el, hogy páros-e.
+"""
+
+# szam1 = int(input("Kérlek Adj meg egy számot: "))
+# if szam1 % 2 == 0 and szam1 > 0:
+#     print("A beírt szám páros")
+# elif szam1 == 0:
+#     print("A beírt szám a 0")
+# else:
+#     print("A beírt szám páratlan")
